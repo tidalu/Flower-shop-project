a2973b0da18e18b818675382953c8e420c730e2a
Tidalu <127156318+tidalu@users.noreply.github.com>
2023-04-28 09:31:47 +0200

    update on bouquet page

--- app.js
+++ app.js
@@ -35,11 +35,22 @@ function updateActive() {
 const rangeSlider = document.querySelector('.range-slider');
 const rangeValues = document.querySelectorAll('.range-value');
 
+const range1 = document.querySelector('#range1');
+const range3 = document.querySelector('#range3');
+
 rangeSlider.addEventListener('input', () => {
     rangeValues.forEach((rangeValue, index) => {
-        rangeValue.innerHTML = rangeSlider.children[index].value + '$';
+        if (range1.value >= range3.value) {
+            rangeValue.innerHTML = range3.value + '$';
+        } else {
+            rangeValue.innerHTML = rangeSlider.children[index].value + '$';
+        }
     });
+
 });
+
+
+
 
 
 // dropdown menu 
@@ -53,10 +64,10 @@ dropdowns.forEach((dropdown) => {
     dropdownButton.addEventListener('click', (e) => {
         dropdownContent.classList.toggle('show');
         dropdownButton.children[1].classList.toggle('rotate');
-        
+
     });
 
-    
+
 
     radioButtons.forEach((radioButton) => {
         radioButton.addEventListener('click', () => {
@@ -69,296 +80,376 @@ dropdowns.forEach((dropdown) => {
 const menu = [
     {
         id: 1,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-1.jpg",
-        description: "designer bouquet",
-        cost: "30", 
+        description: "Designer bouquet",
+        cost: "30",
         size: "S",
     },
     {
         id: 2,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-2.jpg",
-        description: "designer bouquet",
-        cost: "65", 
+        description: "Designer bouquet",
+        cost: "65",
         size: "L",
     },
     {
         id: 3,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-3.jpg",
-        description: "designer bouquet",
-        cost: "100", 
+        description: "Designer bouquet",
+        cost: "100",
         size: "XL",
     },
     {
         id: 4,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-4.jpg",
-        description: "designer bouquet",
-        cost: "29", 
+        description: "Mono bouquets",
+        cost: "29",
         size: "S",
     },
     {
         id: 5,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-5.jpg",
-        description: "designer bouquet",
-        cost: "66", 
+        description: "Mono bouquets",
+        cost: "66",
         size: "M",
     },
     {
         id: 6,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-6.jpg",
-        description: "designer bouquet",
-        cost: "68", 
+        description: "Mono bouquets",
+        cost: "68",
         size: "L",
     },
     {
         id: 7,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-7.jpg",
-        description: "designer bouquet",
-        cost: "90", 
+        description: "Bouquets of dried flowers",
+        cost: "90",
         size: "XL",
     },
     {
         id: 8,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-8.jpg",
-        description: "designer bouquet",
-        cost: "63", 
+        description: "Bouquets of dried flowers",
+        cost: "63",
         size: "M",
     },
     {
         id: 9,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-9.jpg",
-        description: "designer bouquet",
-        cost: "45", 
+        description: "Bouquets of dried flowers",
+        cost: "45",
         size: "S",
     },
     {
         id: 10,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-10.jpg",
-        description: "designer bouquet",
-        cost: "73", 
+        description: "Wedding bouquets",
+        cost: "73",
         size: "L",
     },
     {
         id: 11,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "../images/bouquet/bouquet-11.jpg",
-        description: "designer bouquet",
-        cost: "88", 
+        description: "Wedding bouquets",
+        cost: "88",
         size: "XL",
     },
     {
         id: 12,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-12.jpg",
-        description: "designer bouquet",
-        cost: "39", 
+        description: "Wedding bouquets",
+        cost: "39",
         size: "S",
     },
     {
         id: 13,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-13.jpg",
-        description: "designer bouquet",
-        cost: "55", 
+        description: "Bouquets in boxes",
+        cost: "55",
         size: "M",
     },
     {
         id: 14,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-14.jpg",
-        description: "designer bouquet",
-        cost: "75", 
+        description: "Bouquets in boxes",
+        cost: "75",
         size: "L",
     },
     {
         id: 15,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-15.jpg",
-        description: "designer bouquet",
-        cost: "95", 
+        description: "Bouquets in boxes",
+        cost: "95",
         size: "XL",
     },
     {
         id: 16,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-16.jpg",
-        description: "designer bouquet",
-        cost: "41", 
+        description: "Sweet bouquets",
+        cost: "41",
         size: "S",
     },
     {
         id: 17,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-17.jpg",
-        description: "designer bouquet",
-        cost: "60", 
+        description: "Sweet bouquets",
+        cost: "60",
         size: "M",
     },
     {
         id: 18,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-18.jpg",
-        description: "designer bouquet",
-        cost: "65", 
+        description: "Sweet bouquets",
+        cost: "65",
         size: "L",
     },
     {
         id: 19,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-19.jpg",
-        description: "designer bouquet",
-        cost: "85", 
+        description: "Sweet bouquets",
+        cost: "85",
         size: "XL",
     },
     {
         id: 20,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-20.jpg",
-        description: "designer bouquet",
-        cost: "32", 
+        description: "Bouquets of soap flowers",
+        cost: "32",
         size: "S",
     },
     {
         id: 21,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-21.jpg",
-        description: "designer bouquet",
-        cost: "55", 
+        description: "Bouquets of soap flowers",
+        cost: "55",
         size: "M",
     },
     {
         id: 22,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-22.jpg",
-        description: "designer bouquet",
-        cost: "80", 
+        description: "Bouquets of soap flowers",
+        cost: "80",
         size: "L",
     },
     {
         id: 23,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-23.jpg",
-        description: "designer bouquet",
-        cost: "100", 
+        description: "Boxes of soap flowers",
+        cost: "100",
         size: "XL",
     },
     {
         id: 24,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-24.jpg",
-        description: "designer bouquet",
-        cost: "35", 
+        description: "Boxes of soap flowers",
+        cost: "35",
         size: "S",
     },
     {
         id: 25,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-25.jpg",
-        description: "designer bouquet",
-        cost: "56", 
+        description: "Boxes of soap flowers",
+        cost: "56",
         size: "M",
     },
     {
         id: 26,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-10.jpg",
-        description: "designer bouquet",
-        cost: "70", 
+        description: "Boxes of soap flowers",
+        cost: "70",
         size: "L",
     },
     {
         id: 27,
-        name: "Bouquet 'Sun' ", 
-        category: "peonies", 
+        name: "Bouquet 'Sun' ",
+        category: "peonies",
         image: "./images/bouquet/bouquet-21.jpg",
-        description: "designer bouquet",
-        cost: "90", 
+        description: "Wedding bouquets",
+        cost: "90",
         size: "XL",
     },
 ];
 
 
+window.addEventListener("DOMContentLoaded", function () {
+    const bouquetCards = document.querySelector('.bouquet-cards');
 
-const bouquetCards = document.querySelector('.bouquet-cards');
+    let displayMenu = menu.map(function (item) {
+        return `
+            <div class="bouquet-card">
+                <div class="bouquet-img">
+                    <img src="${item.image}" alt="">
+                </div>
+                <div class="card-name">
+                    <h3>${item.name}</h3>
+                </div>
+                <div class="line"></div>
+                <div class="card-price">
+                    <h3>${item.cost} <span class="currency">USD</span></h3>
+                    <div class="cart-icon">
+                        <i class="fa-solid fa-heart  "></i>
+                        <p class="like-counter">0</p>
+                        <i class="fa-solid fa-cart-shopping shopping"></i>
+                    </div>
+                </div>
+            </div>`;
+    });
 
-window.addEventListener("DOMContentLoaded", function() {
-        let displayMenu = menu.map(function(item) {
-            return `
-                <div class="bouquet-card">
-                    <div class="bouquet-img">
-                        <img src="${item.image}" alt="">
-                    </div>
-                    <div class="card-name">
-                        <h3>${item.name}</h3>
-                    </div>
-                    <div class="line"></div>
-                    <div class="card-price">
-                        <h3>${item.cost} <span class="currency">USD</span></h3>
-                        <div class="cart-icon">
-                            <i class="fa-regular fa-heart"></i>
-                            <i class="fa-solid fa-cart-shopping"></i>
-                        </div>
-                    </div>
-                </div>`;
+    displayMenu = displayMenu.join("");
+    bouquetCards.innerHTML += displayMenu;
+
+    const hearts = document.querySelectorAll('.fa-heart');
+    hearts.forEach(function (heart) {
+        const cartIcon = heart.parentElement;
+        const likeCounter = cartIcon.querySelector('.like-counter');
+        let counterLike = 0;
+        likeCounter.style.display = "none";
+        heart.addEventListener("click", function () {
+            heart.classList.toggle("color");
+            if (heart.classList.contains("color")) {
+                counterLike++;
+            } else {
+                counterLike--;
+            }
+            likeCounter.textContent = counterLike;
+            if (counterLike <= 0) {
+                likeCounter.style.display = "none";
+            } else {
+                likeCounter.style.display = "inline";
+            }
         });
-        displayMenu = displayMenu.join("");
-        bouquetCards.innerHTML += displayMenu
-        
+    });
 });
 
 
-const prevBqt = document.querySelector('.prev-bouquet');
-const nextBqt = document.querySelector('.next-bouquet');
+const categories = document.querySelector('.list');
+///////const prevBqt = document.querySelector('.prev-bouquet');
+//////const nextBqt = document.querySelector('.next-bouquet');
 
+// categories.addEventListener("click", function (e) {
 
-prevBqt.addEventListener("click", function() {
-    
+//     let displayMenu = menu.map(function (item) {
+//         if(item.description === e.currentTarget.textContent){
+//             return `
+//                 <div class="bouquet-card">
+//                     <div class="bouquet-img">
+//                         <img src="${item.image}" alt="">
+//                     </div>
+//                     <div class="card-name">
+//                         <h3>${item.name}</h3>
+//                     </div>
+//                     <div class="line"></div>
+//                     <div class="card-price">
+//                         <h3>${item.cost} <span class="currency">USD</span></h3>
+//                         <div class="cart-icon">
+//                             <i class="fa-solid fa-heart"></i>
+//                             <i class="fa-solid fa-cart-shopping"></i>
+//                         </div>
+//                     </div>
+//                 </div>`;
+//         };
+//     });
+//     displayMenu = displayMenu.join("");
+//     bouquetCards.innerHTML = displayMenu;
+// })
+categories.addEventListener("click", function (e) {
+    let catArr = menu.map(item => {
+        return {
+            description: item.description
+        }
+    });
+    for (let i = 0; i < catArr.length; i++) {
+        if (catArr[i].description === e.currentTarget.textContent) {
+            let displayMenu = catArr.map(function (item) {
+                return `
+                    <div class="bouquet-card">
+                        <div class="bouquet-img">
+                            <img src="${item.image}" alt="">
+                        </div>
+                        <div class="card-name">
+                            <h3>${item.name}</h3>
+                        </div>
+                        <div class="line"></div>
+                        <div class="card-price">
+                            <h3>${item.cost} <span class="currency">USD</span></h3>
+                            <div class="cart-icon">
+                                <button>
+                                <i class="fa-regular fa-heart"></i></button>
+                                <p id="like-counter">0</p>
+                                <button>
+                                <i class="fa-solid fa-cart-shopping"></i></button>
+                            </div>
+                        </div>
+                    </div>`;
+            });
+
+            displayMenu = displayMenu.join("");
+            bouquetCards.innerHTML = displayMenu;
+        }
+    }
 });
 
-nextBqt.addEventListener("click", function() {
-    
-});
 
 
-const  range1 = document.querySelector('#range1');
-const  range3 = document.querySelector('#range3');
+// prevBqt.addEventListener("click", function () {
 
-if(range1.value >= range3.value){
-    range1.max = range1.value;
-    range3.min = range1.max;
-}
+// });
+
+// nextBqt.addEventListener("click", function () {
+
+// });
+
+
